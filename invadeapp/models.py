0affb43993419041bc8ee237043c99871b467f05
notvert <jonathan.pan@gmail.com>
2022-10-14 19:29:43 -0700

    migrating from HB2

---
+++ invadeapp/models.py
@@ -0,0 +1,33 @@
+from django.db import models
+
+class Champions(models.Model):
+    key = models.IntegerField() 
+    name = models.CharField(max_length=200)
+    attack = models.IntegerField() 
+    defense = models.IntegerField() 
+    magic = models.IntegerField() 
+    difficulty = models.IntegerField() 
+    hp = models.FloatField()
+    hpperlevel = models.FloatField()
+    mp = models.FloatField()
+    mpperlevel = models.FloatField()
+    movespeed = models.FloatField()
+    armor = models.FloatField()
+    armorperlevel = models.FloatField()
+    spellblock = models.FloatField()
+    spellblockperlevel = models.FloatField()
+    attackrange = models.FloatField()
+    hpregen = models.FloatField()
+    hpregenperlevel = models.FloatField()
+    mpregen = models.FloatField()
+    mpregenperlevel = models.FloatField()
+    crit = models.FloatField()
+    critperlevel = models.FloatField()
+    attackdamage = models.FloatField()
+    attackdamageperlevel = models.FloatField()
+    attackspeedperlevel = models.FloatField()
+    attackspeed = models.FloatField()
+    image = models.CharField(max_length=100, default='meh')
+
+    def __str__(self):
+        return self.name
